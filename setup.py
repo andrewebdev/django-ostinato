576b9fb276a956df63c078f2648a51932d98ea51
Andre Engelbrecht <andre@teh-node.co.za>
2010-06-01 20:30:14 +0100

    Initial Commit

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name="django-ostinato",
+    description="Django-ostinato brings common CMS features to pluggable django apps",
+    version="0.01",
+    url="",
+    author="Andre Engelbrecht",
+    authot_email="andre@teh-node.co.za",
+    packages=['ostinato'],
+    package_dir={'': 'src'},
+)
