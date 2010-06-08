1ff5bd7e94d6be801cfbe8ed7c3e14265a23d3b1
Andre Engelbrecht <andre@teh-node.co.za>
2010-06-08 13:07:28 +0100

    Added the new ContentItemManager class, and wrote new test to test new methods and functions

---
+++ src/ostinato/managers.py
@@ -0,0 +1,49 @@
+from django.db import models
+
+class ContentItemManager(models.Manager):
+    def _get_parents(self, item):
+        if item.parent:
+            yield item.parent
+            self._get_parents(item.parent)
+
+    def get_nav_bar(self, parent=False):
+        """
+        Returns a dictionary of items with their titles and urls.
+
+        ``parent`` is an instance of ContentItem. If specified, will only
+        return items that has ``parent`` set as the ``parent`` for that item.
+
+        """
+        to_return = []
+        if not parent:
+            nav_items = self.get_query_set().filter(parent=None)
+        else:
+            nav_items = self.get_query_set().filter(parent=parent)
+
+        if nav_items:
+            for item in nav_items:
+                to_return.append({
+                    'title': item.get_short_title(),
+                    'url': item.get_absolute_url(),
+                })
+        return to_return
+
+    def get_breadcrumbs(self, content_item):
+        """
+        Returns the breadcrumbs for the current ``contnet_item``
+
+        """
+        to_return = []
+        parents = [parent for parent in self._get_parents(content_item)]
+        if parents and not(None in parents):
+            to_return += [{
+                'title': parent.get_short_title(),
+                'url': parent.get_absolute_url(),
+                'current': False,
+            } for parent in parents]
+        to_return.append({
+            'title': content_item.get_short_title(),
+            'url': content_item.get_absolute_url(),
+            'current': True,
+        })
+        return to_return
--- src/ostinato/tests.py
+++ src/ostinato/tests.py
@@ -11,12 +11,55 @@ class CMSTestCase(TestCase):
             location="/",
         )
 
-    def testBase(self):
-        self.assertequals(1, 2)
+        self.item2 = ContentItem.objects.create(
+            title="Item2",
+            short_title="Test",
+            description="A second test item",
+            location="/test/",
+            parent=self.item,
+        )
 
 class ContentItemTestCase(CMSTestCase):
     def testItem(self):
+        self.assertEquals(ContentItem.objects.all().count(), 2)
+
+    def testStatus(self):
+        self.assertEquals(self.item.status, self.item.DRAFT,
+            "Incorrect status (%s) for item." % self.item.status)
+        self.assertEquals(self.item.publish_date, None)
+        self.item.action_publish()
+        self.assertEquals(self.item.status, self.item.PUBLISHED,
+            "Incorrect status (%s) for item after action_publish()" % self.item.status)
+        self.failUnless(self.item.publish_date != None,
+            "Publish Date should not be none")
+
+    def testURL(self):
+        self.assertEquals(self.item.get_absolute_url(), '/')
+
+    def testNavBar(self):
+        nav_menu = ContentItem.objects.get_nav_bar()
         self.assertEquals(
-            ContentItem.objects.all().len(),
-            1
+            nav_menu,
+            [{'title': 'Home', 'url': '/'}],
+        )
+        sub_menu = ContentItem.objects.get_nav_bar(parent=self.item)
+        self.assertEquals(
+            sub_menu,
+            [{'title': 'Test', 'url': '/test/'}],
+        )
+
+    def testBreadCrumbs(self):
+        crumbs = ContentItem.objects.get_breadcrumbs(self.item)
+        self.assertEquals(
+            crumbs,
+            [{'title': 'Home', 'url': '/', 'current': True}]
+        )
+
+        crumbs = ContentItem.objects.get_breadcrumbs(self.item2)
+        self.assertEquals(
+            crumbs,
+            [
+                {'title': 'Home', 'url': '/', 'current': False},
+                {'title': 'Test', 'url': '/test/', 'current': True}
+            ]
         )
