5a9403afeae366d823f8f41eaf6c3069a0bd9ded
Andre Engelbrecht <andre@teh-node.co.za>
2010-06-11 10:43:58 +0100

    Created a template tag that helps us to retrieve the the Content Item for the object

--- src/ostinato/templatetags/ostinato_tags.py
+++ src/ostinato/templatetags/ostinato_tags.py
@@ -1,4 +1,5 @@
 from django import template
+from django.contrib.contenttypes.models import ContentType
 
 from ostinato.models import ContentItem
 
@@ -15,3 +16,32 @@ def render_navbar(parent=None):
     else:
         navbar = ContentItem.objects.get_navbar()
     return locals()
+
+class GetContentItemNode(template.Node):
+    def __init__(self, for_object, as_varname):
+        self.for_object = template.Variable(for_object)
+        self.as_varname = as_varname
+
+    def render(self, context):
+        for_object = self.for_object.resolve(context)
+        for_object_type = ContentType.objects.get_for_model(for_object)
+        try:
+            content_item = ContentItem.objects.get(
+                content_type=for_object_type,
+                object_id=for_object.id
+            )
+        except:
+            content_item = None
+        context[self.as_varname] = content_item
+        return ''
+
+@register.tag()
+def get_cmsitem(parser, token):
+    tokens = token.contents.split()
+    if len(tokens) != 5:
+        raise template.TemplateSyntaxError("Incorrect number of arguments for %r tag" % tokens[0])
+    if tokens[1] != 'for':
+        raise template.TemplateSyntaxError("Second argument in %r tag must be 'for'" % tokens[0])
+    if tokens[3] != 'as':
+        raise template.TemplateSyntaxError("Forth argument in %r tag must be 'as'" % tokens[0])
+    return GetContentItemNode(for_object=tokens[2], as_varname=tokens[4])
