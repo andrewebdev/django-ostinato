16589216d35cc229265b0a2b9fec7578973926ab
Andre Engelbrecht <andre@teh-node.co.za>
2010-12-24 18:45:18 +0000

    Started adding doctests since its more descriptive and will help with the final documentation.

--- src/ostinato/tests.py
+++ src/ostinato/tests.py
@@ -77,4 +77,37 @@ class ContentItemTestCase(CMSTestCase):
 __test__ = {
 "doctest": """
 
+First we create some flatpages which will be our content to work with.
+
+    >>> homepage = FlatPage.objects.create(url="/", title="Home", content="Lorem Ipsum dolor set...")
+    >>> aboutus = FlatPage.objects.create(url="/about-us/", title="About Us", content="Lorem Ipsum dolor set...")
+    >>> contact = FlatPage.objects.create(url="/contact/", title="Contact Us", content="Lorem Ipsum dolor set...")
+
+We can create ContentItem instances, and specify the location for these
+items.
+
+    >>> os_homepage = ContentItem.objects.create(title="Home", description="The Home Page", location="/")
+    >>> os_contact = ContentItem.objects.create(title="Contact Us", short_title="Contact", description="Contact us page", location="/contact/", parent=os_homepage)
+
+We can also create a ContentItem that is generically related to another
+ContentType, like a flatpage.
+
+    >>> os_aboutus = ContentItem.objects.create_for(aboutus, title="About Us", short_title="About", description="About Us Page", parent=os_homepage)
+
+The url for a ContentItem can be looked up using ``get_absolute_url()``
+
+    >>> os_homepage.get_absolute_url()
+    '/'
+    >>> os_contact.get_absolute_url()
+    '/contact/'
+    >>> os_aboutus.get_absolute_url()
+    u'/about-us/'
+
+ContentItem.get_absolute_url() is 'smart'. If the ContentItem is
+generically related to another contenttype that has a
+``get_absolute_url()`` method, then it will automatically use the url
+returned from that original method. If it cannot find a
+get_absolute_url() method on the target contenttype, then it will fall
+back to the url specified in the ``location`` field.
+
 """}
