2dc6cf9937ee8c8a80b465ed8ff3aa90f0b6031a
Andre Engelbrecht <andre@teh-node.co.za>
2010-12-24 17:24:53 +0000

    Get some tests in place for new features we will be developing soon. We need some basic conent objects, and will use flatpages for now

--- src/ostinato/tests.py
+++ src/ostinato/tests.py
@@ -5,61 +5,86 @@ from ostinato.models import ContentItem
 
 class CMSTestCase(TestCase):
     def setUp(self):
-        self.item = ContentItem.objects.create(
+        # Create some basic test content
+        self.homepage = FlatPage.objects.create(
+            url="/",
             title="Home",
-            description="A short description of the content",
-            location="/",
+            content="Lorem Ipsum dolor set..."
+        )
+        self.aboutus = FlatPage.objects.create(
+            url="/about-us/",
+            title="About Us",
+            content="Lorem Ipsum dolor set..."
+        )
+        self.contact = FlatPage.objects.create(
+            url="/contact/",
+            title="Contact Us",
+            content="Lorem Ipsum dolor set..."
         )
 
-        self.item2 = ContentItem.objects.create(
-            title="Item2",
-            short_title="Test",
-            description="A second test item",
-            location="/test/",
-            parent=self.item,
+        # Define a standard ContentItem
+        self.os_homepage = ContentItem.objects.create(
+            title="Home",
+            description="The Home Page",
+            location="/",
+        )
+        # Define a ContentItem with a parent and custom location
+        self.os_aboutus = ContentItem.objects.create(
+            title="About Us",
+            short_title="About",
+            description="About Us Page",
+            location="/about-us/",
+            parent=self.os_homepage,
+        )
+        # Define a ContentItem with a parent and pointing to another
+        # django app
+        self.os_contact = ContentItem.objects.create(
+            title="Contact Us",
+            short_title="Contact",
+            description="Contact us page",
+            location="/contact/",
+            parent=self.os_homepage,
         )
 
 class ContentItemTestCase(CMSTestCase):
-    def testItem(self):
-        self.assertEquals(ContentItem.objects.all().count(), 2)
+    # def testStatus(self):
+    #     self.assertEquals(self.item.status, self.item.DRAFT,
+    #         "Incorrect status (%s) for item." % self.item.status)
+    #     self.assertEquals(self.item.publish_date, None)
+    #     self.item.action_publish()
+    #     self.assertEquals(self.item.status, self.item.PUBLISHED,
+    #         "Incorrect status (%s) for item after action_publish()" % self.item.status)
+    #     self.failUnless(self.item.publish_date != None,
+    #         "Publish Date should not be none")
 
-    def testStatus(self):
-        self.assertEquals(self.item.status, self.item.DRAFT,
-            "Incorrect status (%s) for item." % self.item.status)
-        self.assertEquals(self.item.publish_date, None)
-        self.item.action_publish()
-        self.assertEquals(self.item.status, self.item.PUBLISHED,
-            "Incorrect status (%s) for item after action_publish()" % self.item.status)
-        self.failUnless(self.item.publish_date != None,
-            "Publish Date should not be none")
-
-    def testURL(self):
-        self.assertEquals(self.item.get_absolute_url(), '/')
+    # def testURL(self):
+    #     self.assertEquals(self.item.get_absolute_url(), '/')
 
     def testNavBar(self):
-        nav_menu = ContentItem.objects.get_navbar()
+        root = ContentItem.objects.get_navbar()
         self.assertEquals(
-            nav_menu,
+            root,
             [{'title': 'Home', 'url': '/'}],
         )
-        sub_menu = ContentItem.objects.get_navbar(parent=self.item)
+        sub = ContentItem.objects.get_navbar(parent=self.os_homepage)
         self.assertEquals(
-            sub_menu,
-            [{'title': 'Test', 'url': '/test/'}],
+            sub,
+            [{'title': 'About', 'url': '/about-us/'},
+             {'title': 'Contact', 'url': '/contact/'}],
         )
 
     def testBreadCrumbs(self):
-        crumbs = ContentItem.objects.get_breadcrumbs(self.item)
+        crumbs = ContentItem.objects.get_breadcrumbs(self.os_aboutus)
         self.assertEquals(
             crumbs,
-            [{'title': 'Home', 'url': '/', 'current': True}]
-        )
-
-        crumbs = ContentItem.objects.get_breadcrumbs(self.item2)
-        self.assertEquals(
-            crumbs,
-            [
-                {'title': 'Home', 'url': '/', 'current': False},
-                {'title': 'Test', 'url': '/test/', 'current': True}
+            [{'title': 'Home', 'url': '/', 'current': False},
+             {'title': 'About', 'url': '/about-us/',
+              'current': True}
             ]
         )
+
+## Doctests are nice for documentation :)
+__test__ = {
+"doctest": """
+
+"""}
