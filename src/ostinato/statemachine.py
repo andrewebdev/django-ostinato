af1f212440e88e33f5b5784a28766015c5e433be
Andre Engelbrecht <andre@teh-node.co.za>
2011-01-20 10:41:33 +0000

    Statemachine fields updated to have the sm_ prefix. Removed the pre_get_state and pre_set_action methods. If we want extra functionality here, we can just override the _set_state() and _get_state() methods directly.

--- src/ostinato/statemachine.py
+++ src/ostinato/statemachine.py
@@ -15,21 +15,24 @@ class InvalidAction(Exception):
     def __str__(self):
         return repr(self.value)
 
-pre_action = Signal(providing_args=['action', 'user'])
-post_action = Signal(providing_args=['action', 'user'])
+sm_pre_action = Signal(providing_args=['action', 'user'])
+sm_post_action = Signal(providing_args=['action', 'user'])
 
 class StateMachine(object):
     """
     Any model that wishes to use this statemachine can simply
     inherrit from it.
 
-    Note that this model assumes you have a ``sm_state`` field
+    Note that this model assumes you have a ``_sm_state`` CharField
     defined in your main model.
+
+    TODO: How to handle permissions?
     """
     SM_ACTIONS = [
         {'action': 'submit',
          'help_text': 'Submit document for review',
-         'target': 'review'
+         'target': 'review',
+         'groups': '', # This is how we will define group permissions for now
         },
         {'action': 'publish',
          'help_text': 'Publish this document',
@@ -52,14 +55,9 @@ class StateMachine(object):
     ]
 
     def _get_state(self):
-        self.pre_get_state()
         return self._sm_state
-
     def _set_state(self, what):
-        self.pre_set_state()
         self._sm_state = what
-        self.save()
-
     sm_state = property(_get_state, _set_state)
 
     def sm_state_actions(self):
@@ -77,43 +75,34 @@ class StateMachine(object):
         for item in self.SM_STATEMACHINE:
             if state == item['state']: return item
 
-    def sm_take_action(self, action, user=None):
+    def sm_take_action(self, action, **kwargs):
         """
         Take an action to change the state and send the apropriate signals.
+
+        ``kwargs`` can be used to pass along extra data to the ``pre_action()``
+        and ``post_action()`` methods, and is ideal for passing along data to
+        update other fields in the model before saving.
         """
         if action in self.sm_state_actions():
-            self.sm_pre_action(action, user)
+            self.sm_pre_action(action=action, **kwargs)
             self.sm_state = self._get_action(action)['target']
-            self.sm_post_action(action, user)
+            self.sm_post_action(action=action, **kwargs)
         else:
             raise InvalidAction('Invalid action, %s. Choices are, %s' % (
                 action, ','.join(self.sm_state_actions())))
 
-    def pre_get_state(self):
-        """
-        Can be used to do some extra stuff like formatting text, or even
-        changing state before we show the state.
-        """
-        pass
-
-    def pre_set_state(self):
-        """
-        Can be used to do some extra stuff before setting the state
-        """
-        pass
-
-    def sm_pre_action(self, *args, **kwargs):
+    def sm_pre_action(self, **kwargs):
         """
         Method can be used to do some extra stuff to our model before the
         action is taken and saved.
         Just override in your model if you need it.
         """
-        pre_action.send(sender=self, instance=self, **kwargs)
+        sm_pre_action.send(sender=self, instance=self, **kwargs)
 
-    def sm_post_action(self, *args, **kwargs):
+    def sm_post_action(self, **kwargs):
         """
         Method can be used to do some extra stuff to our model after the
         action is saved.
         Just override in your model if you need it.
         """
-        post_action.send(sender=self, instance=self, **kwargs)
+        sm_post_action.send(sender=self, instance=self, **kwargs)
