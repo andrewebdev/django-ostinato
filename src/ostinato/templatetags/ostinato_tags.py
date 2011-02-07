6ce1f29bc9e6b244c97b432d2fe9ca3ec704d668
Andre Engelbrecht <andre@teh-node.co.za>
2011-02-07 10:05:48 +0200

    Added an active_link template tag so that we can use this to mark active links on pages

--- src/ostinato/templatetags/ostinato_tags.py
+++ src/ostinato/templatetags/ostinato_tags.py
@@ -1,3 +1,5 @@
+import re
+
 from django import template
 from django.contrib.contenttypes.models import ContentType
 
@@ -18,6 +20,24 @@ def render_navbar(parent=None):
         navbar = ContentItem.objects.get_navbar()
     return locals()
 
+@register.simple_tag
+def active_link(request, pattern):
+    if request:
+        if re.search(pattern, request.path):
+            return 'active'
+    return ''
+
+@register.tag()
+def get_cmsitem(parser, token):
+    tokens = token.contents.split()
+    if len(tokens) != 5:
+        raise template.TemplateSyntaxError("Incorrect number of arguments for %r tag" % tokens[0])
+    if tokens[1] != 'for':
+        raise template.TemplateSyntaxError("Second argument in %r tag must be 'for'" % tokens[0])
+    if tokens[3] != 'as':
+        raise template.TemplateSyntaxError("Forth argument in %r tag must be 'as'" % tokens[0])
+    return GetContentItemNode(for_object=tokens[2], as_varname=tokens[4])
+
 class GetContentItemNode(template.Node):
     def __init__(self, for_object, as_varname):
         self.for_object = template.Variable(for_object)
@@ -30,14 +50,3 @@ class GetContentItemNode(template.Node):
             content_type=for_object_type, object_id=for_object.id)
         context[self.as_varname] = content_item
         return ''
-
-@register.tag()
-def get_cmsitem(parser, token):
-    tokens = token.contents.split()
-    if len(tokens) != 5:
-        raise template.TemplateSyntaxError("Incorrect number of arguments for %r tag" % tokens[0])
-    if tokens[1] != 'for':
-        raise template.TemplateSyntaxError("Second argument in %r tag must be 'for'" % tokens[0])
-    if tokens[3] != 'as':
-        raise template.TemplateSyntaxError("Forth argument in %r tag must be 'as'" % tokens[0])
-    return GetContentItemNode(for_object=tokens[2], as_varname=tokens[4])
