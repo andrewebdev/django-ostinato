aff272cffe787cc92892500c29564b9be8b1f6fa
Andre Engelbrecht <andre@teh-node.co.za>
2012-01-16 11:58:53 +0000

    Updated the statemachine tests to ignore ostinato fixtures, and use it's own. (Since the Statemachine might be used without other ostinato apps)

--- src/ostinato/statemachine/tests.py
+++ src/ostinato/statemachine/tests.py
@@ -5,33 +5,40 @@ from django.contrib.contenttypes.models import ContentType
 from django.template import Context, Template
 from django.template.response import SimpleTemplateResponse
 
-from ostinato.models import ContentItem
 from models import StateMachineBase, DefaultStateMachine, StateMachineField
 from models import InvalidAction
 from models import sm_pre_action, sm_post_action
 from templatetags.statemachine_tags import GetStateMachineNode
 
 
-class _DummyModel(models.Model):
+class ContentItem(models.Model):
     name = models.CharField(max_length=50)
     pre = models.BooleanField(default=False)
     post = models.BooleanField(default=False)
     statemachine = StateMachineField(DefaultStateMachine)
 
 
-class StateMachineBaseModelTestCase(TestCase):
+class _SetupMixin(object):
 
-    fixtures = ['ostinato_test_fixtures.json']
+    def setUp(self):
+        ContentItem.objects.create(name='Content Item 1')
+        ContentItem.objects.create(name='Content Item 2')
+        ContentItem.objects.create(name='Content Item 3')
+
+
+class StateMachineBaseModelTestCase(_SetupMixin, TestCase):
 
     def setUp(self):
         self.content_item_type = ContentType.objects.get(
-            app_label='ostinato', model='contentitem')
+            app_label='statemachine', model='contentitem')
 
         self.sm = StateMachineBase(
             state='private',
             content_type=self.content_item_type,
             object_id=1,
         )
+        super(StateMachineBaseModelTestCase, self).setUp()
+
         
     def test_model_exists(self):
         pass
@@ -51,13 +58,13 @@ class StateMachineBaseModelTestCase(TestCase):
         self.assertEqual('content item, 1 (private)', self.sm.__unicode__())
 
 
-class DefaultStateMachineTestCase(TestCase):
-
-    fixtures = ['ostinato_test_fixtures.json']
+class DefaultStateMachineTestCase(_SetupMixin, TestCase):
 
     def setUp(self):
+        super(DefaultStateMachineTestCase, self).setUp()
+
         self.content_item_type = ContentType.objects.get(
-            app_label='ostinato', model='contentitem')
+            app_label='statemachine', model='contentitem')
 
         self.sm = DefaultStateMachine.objects.create(
             content_type=self.content_item_type, object_id=1)
@@ -144,13 +151,13 @@ class DefaultStateMachineTestCase(TestCase):
         self.assertListEqual(expected_perms, perms)
 
 
-class StateMachineSignalsTestCase(TestCase):
-
-    fixtures = ['ostinato_test_fixtures.json']
+class StateMachineSignalsTestCase(_SetupMixin, TestCase):
 
     def setUp(self):
+        super(StateMachineSignalsTestCase, self).setUp()
+
         self.content_item_type = ContentType.objects.get(
-            app_label='ostinato', model='contentitem')
+            app_label='statemachine', model='contentitem')
 
         self.sm = DefaultStateMachine.objects.create(
             state='private', content_type=self.content_item_type, object_id=1)
@@ -159,64 +166,61 @@ class StateMachineSignalsTestCase(TestCase):
     def test_pre_action_signal(self):
 
         def signal_listner(sender, **kwargs):
-            sender.content_object.show_in_nav = True
+            sender.content_object.pre = True
             sender.content_object.save()
 
         # Connect and send
         sm_pre_action.connect(signal_listner)
 
         self.sm.take_action('submit')
-        self.assertTrue(ContentItem.objects.get(id=1).show_in_nav)
+        self.assertTrue(ContentItem.objects.get(id=1).pre)
+        self.assertFalse(ContentItem.objects.get(id=1).post)
 
     def test_post_action_signal(self):
 
         def signal_listner(sender, **kwargs):
-            sender.content_object.show_in_nav = True
+            sender.content_object.post = True
             sender.content_object.save()
 
         sm_post_action.connect(signal_listner)
 
         self.sm.take_action('publish')
-        self.assertTrue(ContentItem.objects.get(id=1).show_in_nav)
+        self.assertTrue(ContentItem.objects.get(id=1).post)
+        self.assertFalse(ContentItem.objects.get(id=1).pre)
 
 
-class StateMachineFieldTestCase(TestCase):
-
-    fixtures = ['ostinato_test_fixtures.json']
-
-    def setUp(self):
-        self.dummy = _DummyModel(name="Dummy 1")
-        self.dummy.save()
+class StateMachineFieldTestCase(_SetupMixin, TestCase):
 
     def test_statemachine_field_exists(self):
         field = StateMachineField(DefaultStateMachine)
 
     def test_can_get_statemachine_from_field(self):
-        self.assertEqual('private', self.dummy.statemachine.state)
+        ci = ContentItem.objects.get(id=1)
+        self.assertEqual('private', ci.statemachine.state)
 
     def test_can_take_actions(self):
-        self.dummy.statemachine.take_action('publish')
-        self.assertEqual('published', self.dummy.statemachine.state)
+        ci = ContentItem.objects.get(id=1)
+        ci.statemachine.take_action('publish')
+        self.assertEqual('published', ci.statemachine.state)
 
 
-class StateMachineManagerTestCase(TestCase):
-
-    fixtures = ['ostinato_test_fixtures.json']
+class StateMachineManagerTestCase(_SetupMixin, TestCase):
 
     def setUp(self):
-        dummy = _DummyModel(name='Dummy 1')
-        dummy.save()
-        dummy.statemachine.take_action('publish')
+        super(StateMachineManagerTestCase, self).setUp()
+
+        ci = ContentItem.objects.get(id=1)
+        ci.statemachine.take_action('publish')
 
     def test_get_statemachine(self):
         sm = DefaultStateMachine.objects.get_statemachine(
-            ContentItem.objects.get(id=1))
+            ContentItem.objects.get(id=2))
         self.assertEqual(2, DefaultStateMachine.objects.all().count())
 
     def test_filter_has_state_returns_all(self):
-        # Other than dummymodel, we want a seperate content object as well
+        # Other than ci model, we want a seperate content object as well
         sm = DefaultStateMachine.objects.get_statemachine(
-            ContentItem.objects.get(id=1))
+            ContentItem.objects.get(id=2))
         sm.take_action('publish')
 
         qs = DefaultStateMachine.objects.has_state('published')
@@ -224,20 +228,18 @@ class StateMachineManagerTestCase(TestCase):
         self.assertEqual([1, 2], qs)
 
     def test_filter_has_state_with_extra_filter_args(self):
-        # Other than dummymodel, we want a seperate content object as well
+        # Other than cimodel, we want a seperate content object as well
         sm = DefaultStateMachine.objects.get_statemachine(
-            ContentItem.objects.get(id=1))
+            ContentItem.objects.get(id=3))
         sm.take_action('publish')
 
         qs = DefaultStateMachine.objects.has_state('published',
-            content_type__model='_dummymodel')
+            content_type__model='contentitem')
         qs = list(qs.values_list('id', flat=True))
-        self.assertEqual([1], qs)
+        self.assertEqual([1, 2], qs)
 
 
-class GetStateMachineTemplateTagTestCase(TestCase):
-
-    fixtures = ['ostinato_test_fixtures.json']
+class GetStateMachineTemplateTagTestCase(_SetupMixin, TestCase):
 
     def test_template_tag_renders(self):
         template = Template('{% load statemachine_tags %}{% get_statemachine statemachine.defaultstatemachine for item %}{{ statemachine.state }}')
