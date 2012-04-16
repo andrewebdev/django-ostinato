e5c54ee1aeb8194a5d370f18764e5bc488defa20
Andre Engelbrecht <andre@teh-node.co.za>
2012-04-16 13:47:04 +0100

    Cleanup

--- src/ostinato/templatetags/ostinato_tags.py
+++ src/ostinato/templatetags/ostinato_tags.py
@@ -1,27 +1,9 @@
 import re
 
 from django import template
-from django.contrib.contenttypes.models import ContentType
-from django.contrib.auth.models import User
 
-from ostinato.models import ContentItem
-from ostinato.forms import ContentItemForm
 
 register = template.Library()
-
-
-@register.inclusion_tag('ostinato/tags/navbar.html')
-def navbar(parent=None):
-    """
-    Renders the standard navigation bar.
-    ``parent`` specifies the start level for the navbar
-    ``depth`` specifies how deep we should show navbar elements.
-    """
-    if parent:
-        navbar = ContentItem.objects.get_navbar(parent=parent)
-    else:
-        navbar = ContentItem.objects.get_navbar()
-    return locals()
 
 
 @register.simple_tag
@@ -32,31 +14,3 @@ def active_link(request, pattern):
     return ''
 
 
-@register.tag()
-def get_cmsitem(parser, token):
-    tokens = token.contents.split()
-    if len(tokens) != 5:
-        raise template.TemplateSyntaxError("Incorrect number of arguments for %r tag" % tokens[0])
-    if tokens[1] != 'for':
-        raise template.TemplateSyntaxError("Second argument in %r tag must be 'for'" % tokens[0])
-    if tokens[3] != 'as':
-        raise template.TemplateSyntaxError("Forth argument in %r tag must be 'as'" % tokens[0])
-    return GetContentItemNode(for_object=tokens[2], as_varname=tokens[4])
-
-
-class GetContentItemNode(template.Node):
-    def __init__(self, for_object, as_varname):
-        self.for_object = template.Variable(for_object)
-        self.as_varname = as_varname
-
-    def render(self, context):
-        for_object = self.for_object.resolve(context)
-        for_object_type = ContentType.objects.get_for_model(for_object)
-        content_item, created = ContentItem.objects.get_or_create(
-            content_type=for_object_type, object_id=for_object.id)
-        if created:
-            title = getattr(for_object, 'title', repr(for_object))
-            content_item.title = title
-            content_item.save()
-        context[self.as_varname] = content_item
-        return ''
