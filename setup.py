9fffd798178629ada304c63879fb66fe75eda1fb
Andre Engelbrecht <andre@teh-node.co.za>
2012-05-23 11:57:56 +0100

    Moved the ostinato packages out of the src folder. Updated setup.py to reflect this. Also changed setup.py to make use of distutils, rather than setuptools.
    Setup now uses the version specified in the ostinato package.

--- setup.py
+++ setup.py
@@ -1,14 +1,15 @@
-from setuptools import setup, find_packages
+from distutils.core import setup
+
+version = '.'.join([str(i) for i in __import__('ostinato').VERSION])
 
 setup(
     name="django-ostinato",
-    version="0.6.2",
+    version=version,
     url="",
     description="Django-ostinato brings common CMS features to pluggable django apps",
     author="Andre Engelbrecht",
     author_email="andre@teh-node.co.za",
-    packages=find_packages('src'),
-    package_dir={'': 'src'},
+    packages=['ostinato'],
     install_requires=[
         'setuptools',
         'django-mptt == 0.5.2',
