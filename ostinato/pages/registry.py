ad3b13fc5fdffd8bc21213ec6d4c45b5f5959022
Andre Engelbrecht <andre@teh-node.co.za>
2012-06-18 21:45:39 +0100

    Added a fallback SortedRegistry() class which can be used safely until appregister on pypi has been updated

--- ostinato/pages/registry.py
+++ ostinato/pages/registry.py
@@ -1,28 +1,39 @@
 from django.utils.datastructures import SortedDict
 from appregister import Registry
 
+try:
+    from appregister import SortedRegistry
+except ImportError:
 
-class ContentRegister(Registry):
+    class SortedRegistry(Registry):
+
+        def setup(self):
+            """
+            Override the setup method so that we can use a ``list()`` instead of
+            the default ``set()``
+            """
+            self._registry = list()
+
+        def register(self, class_):
+
+            if not self.is_valid(class_):
+                msg = "Object '%s' is not a subclass of '%s'" % (class_.__name__,
+                    self.base.__name__)
+                raise InvalidOperation(msg)
+
+            if self.is_registered(class_):
+                msg = "Object '%s' has already been registered" % class_.__name__
+                raise AlreadyRegistered(msg)
+
+            self._registry.append(class_)
+
+            return class_
+
+
+
+class ContentRegister(SortedRegistry):
     base = 'ostinato.pages.models.PageContent'
     discovermodule = 'models'
-
-    def setup(self):
-        self._registry = list()
-
-    def register(self, class_):
- 
-        if not self.is_valid(class_):
-            msg = "Object '%s' is not a subclass of '%s'" % (class_.__name__,
-                self.base.__name__)
-            raise InvalidOperation(msg)
- 
-        if self.is_registered(class_):
-            msg = "Object '%s' has already been registered" % class_.__name__
-            raise AlreadyRegistered(msg)
- 
-        self._registry.append(class_)
- 
-        return class_
 
     def get_template_choices(self):
         template_choices = (('', '--------'),)
