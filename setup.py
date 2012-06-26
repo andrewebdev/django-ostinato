d3af0be788bf6eed27200588bed25c47d6291d31
Andre Engelbrecht <andre@teh-node.co.za>
2012-06-26 11:19:37 +0100

    Working on the pypi packaging

--- setup.py
+++ setup.py
@@ -1,7 +1,46 @@
+import os
 from distutils.core import setup
+
 
 version = '.'.join([str(i) for i in __import__('ostinato').VERSION])
 
+
+# Snippets borrowed from django setup.py ...
+def fullsplit(path, result=None):
+    """
+    Split a pathname into components (the opposite of os.path.join) in a
+    platform-neutral way.
+    """
+    if result is None:
+        result = []
+    head, tail = os.path.split(path)
+    if head == '':
+        return [tail] + result
+    if head == path:
+        return result
+    return fullsplit(head, [tail] + result)
+
+
+# Snippets borrowed from django setup.py ...
+# Compile the list of packages available, because distutils doesn't have
+# an easy way to do this.
+packages, data_files = [], []
+root_dir = os.path.dirname(__file__)
+if root_dir != '':
+    os.chdir(root_dir)
+ostinato_dir = 'ostinato'
+
+for dirpath, dirnames, filenames in os.walk(ostinato_dir):
+    # Ignore dirnames that start with '.'
+    for i, dirname in enumerate(dirnames):
+        if dirname.startswith('.'): del dirnames[i]
+    if '__init__.py' in filenames:
+        packages.append('.'.join(fullsplit(dirpath)))
+    elif filenames:
+        data_files.append([dirpath, [os.path.join(dirpath, f) for f in filenames]])
+
+
+# Now our setup
 setup(
     name='django-ostinato',
     version=version,
@@ -9,13 +48,10 @@ setup(
     description='Django-ostinato brings cms-like features to your django projects.',
     author='Andre Engelbrecht',
     author_email='andre@teh-node.co.za',
+    license='MIT',
     download_url='https://github.com/andrewebdev/django-ostinato/tarball/master',
-    packages=[
-        'ostinato',
-        'ostinato.pages',
-        'ostinato.statemachine',
-        'ostinato.contentfilters',
-    ],
+    packages=packages,
+    data_files=data_files,
     keywords=['django', 'cms', 'ostinato'],
     install_requires=[
         'setuptools',
