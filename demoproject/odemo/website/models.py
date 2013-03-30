6787d10f91e6fba3cba88d27550a483a2de16e07
Andre Engelbrecht <andre@tehnode.co.uk>
2013-03-30 19:08:14 +0000

    Created some basic page content.

--- demoproject/odemo/website/models.py
+++ demoproject/odemo/website/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+from ostinato.pages.models import PageContent
+from ostinato.pages.registry import page_content
+
+
+@page_content.register
+class HomePage(PageContent):
+    content = models.TextField()
+
+
+@page_content.register
+class GenericPage(PageContent):
+    content = models.TextField()
