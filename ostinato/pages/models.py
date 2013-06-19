195542f46e23a546e7078dae2864ce61f5926abb
Andre Engelbrecht <andre@tehnode.co.uk>
2013-06-19 14:49:53 +0100

    fixup

--- ostinato/pages/models.py
+++ ostinato/pages/models.py
@@ -2,7 +2,6 @@ import re
 
 from django.db import models
 from django.contrib.auth.models import User
-from django.contrib.contenttypes.models import ContentType
 from django.core.urlresolvers import reverse
 from django.core.cache import get_cache
 from django.utils import timezone
