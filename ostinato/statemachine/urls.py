964c339abb547b85c99ac278b7bae7497aab9bbe
Andre Engelbrecht <andre@tehnode.co.uk>
2016-04-06 21:10:10 +0100

    Statemachine now support django 1.9

--- ostinato/statemachine/urls.py
+++ ostinato/statemachine/urls.py
@@ -1,8 +1,9 @@
-from django.conf.urls import patterns, url
+from django.conf.urls import url
 from ostinato.statemachine.views import StateActionView
 
 
-urlpatterns = patterns('',
+urlpatterns = [
     url(r'^(?P<app_label>\w+)/(?P<model>\w+)/(?P<obj_id>\d+)/$',
         StateActionView.as_view(), name='statemachine_action'),
-)
+]
+
