88957200d5f28fbc4d1e2dc52c27266c9ad4bee4
Andre Engelbrecht <andre@tehnode.co.uk>
2016-04-06 21:09:08 +0100

    Updated blog to be django 1.9 compatable

---
+++ ostinato/blog/apps.py
@@ -0,0 +1,8 @@
+from django.apps import AppConfig
+
+
+class OstinatoBlogConfig(AppConfig):
+    name = 'ostinato.blog'
+    label = 'ost_blog'
+    verbose_name = 'Ostinato Blog Engine'
+
--- ostinato/blog/managers.py
+++ ostinato/blog/managers.py
@@ -2,5 +2,7 @@ from django.db import models
 
 
 class BlogEntryManager(models.Manager):
+
     def published(self):
-        return self.get_query_set().filter(state=5)
+        return self.get_queryset().filter(state=5)
+
