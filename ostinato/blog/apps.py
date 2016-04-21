631665a8aeee54d5094480ddf4140a61dce4a960
Andre Engelbrecht <andre@tehnode.co.uk>
2016-04-21 20:56:53 +0100

    Correct app label of ostinato_blog

--- ostinato/blog/apps.py
+++ ostinato/blog/apps.py
@@ -3,6 +3,6 @@ from django.apps import AppConfig
 
 class OstinatoBlogConfig(AppConfig):
     name = 'ostinato.blog'
-    label = 'ost_blog'
+    label = 'ostinato_blog'
     verbose_name = 'Ostinato Blog Engine'
 
