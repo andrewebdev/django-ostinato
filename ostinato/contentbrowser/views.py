480dc28eb77a10761ca93dbe3d26608f18f1d443
Andre Engelbrecht <andre@tehnode.co.uk>
2016-07-03 12:26:42 +0100

    Content browsers should only be visible to staff

--- ostinato/contentbrowser/views.py
+++ ostinato/contentbrowser/views.py
@@ -1,5 +1,7 @@
 from django import http
 from django.views.generic import TemplateView
+from django.utils.decorators import method_decorator
+from django.contrib.admin.views.decorators import staff_member_required
 from .core import get_browsers, BrowserNotFound
 
 
@@ -16,6 +18,10 @@ class BrowserView(TemplateView):
     description = ""
     browser_id = None
     template_name = None
+
+    @method_decorator(staff_member_required)
+    def dispatch(self, request, *args, **kwargs):
+        return super(BrowserView, self).dispatch(request, *args, **kwargs)
 
     def get_context_data(self, **kwargs):
         c = super(BrowserView, self).get_context_data(**kwargs)
