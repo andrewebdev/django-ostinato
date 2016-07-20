1abe9fbd6b7a24c3e94946bb984f7810a93d9fdb
Andre Engelbrecht <andre@tehnode.co.uk>
2016-07-20 03:18:46 +0100

    Added in a helper from one of the experimental branches, since I use it so often

--- ostinato/utils.py
+++ ostinato/utils.py
@@ -1,5 +1,11 @@
 import time
 
+from django.contrib.sites.models import Site
+from django.template.loader import render_to_string
+from django.utils.html import strip_tags
+from django.core.mail import EmailMultiAlternatives
+
+
 class benchmark(object):
     """
     A simple benchmarking class borrowed from:
@@ -16,3 +22,43 @@ class benchmark(object):
         print("%s : %0.3f seconds" % (self.name, end-self.start))
         return False
 
+
+class Emailer(object):
+    """
+    A simple class to take care of some repetative code that I end up
+    writing for almost every project.
+    """
+    def __init__(self, **kwargs):
+        self.recipients = kwargs['recipients']
+        self.from_address = kwargs['from_address']
+        self.subject_template = kwargs['subject_template']
+        self.body_template = kwargs['body_template']
+        self.context = kwargs['context']
+        self.bcc = kwargs.get('bcc', None)
+
+    def get_context(self):
+        return self._context
+
+    def set_context(self, context={}):
+        if 'site' not in context:
+            context['site'] = Site.objects.get_current()
+        self._context = context
+
+    context = property(get_context, set_context)
+
+    def render(self, template):
+        return render_to_string(template, self.context)
+
+    def send(self, as_html=False):
+        subject = self.render(self.subject_template).replace('\n', ' ')
+        html_content = self.render(self.body_template)
+        text_content = strip_tags(html_content)
+
+        # create the email, and attach the HTML version as well.
+        msg = EmailMultiAlternatives(
+            subject, text_content, self.from_address, self.recipients,
+            bcc=self.bcc)
+        if as_html:
+            msg.attach_alternative(html_content, "text/html")
+
+        msg.send()
